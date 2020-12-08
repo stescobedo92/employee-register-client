6d38debd885956c629ee5a0f89823573f9e0c57a
Sergio Triana <stescobedo.31@gmail.com>
2020-12-08 14:15:11 -0500

    Added the card component in the form

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,11 +3,15 @@ import React from 'react';
 export default function Employee() {
     return(
         <>
-            <di>
-                employee form
+            <di className="container text-center">
+                <p className="lead">An Employee</p>
             </di>
             <form autocomplete="off" novalidate>
-
+                <div className="card">
+                    <div className="card-body">
+                    
+                    </div> 
+                </div>
             </form>
         </>
     )
