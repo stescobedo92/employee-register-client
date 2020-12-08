c0c075c703432dd5542c1135f2b6972970b6776c
Sergio Triana <stescobedo.31@gmail.com>
2020-12-08 13:58:44 -0500

    Added employee component in the employee list component

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 export default function EmployeeList(){
     return(
-        <div>
-            
+        <div className="row">
+            <div className="col-md-4">
+                <Employee />
+            </div>
+            <div className="col-md-8">
+                <div>List of employee records</div>
+            </div>
+
         </div>
     )
 }
